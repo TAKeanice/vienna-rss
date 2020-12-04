edfd0158f2420a9b468cdb102f981479539a4d00
nielspl <ezzora>
2020-12-04 10:07:41 +0100

    Rewrite RichXMLParserTests to Swift.
    This test isn't doing anything but keep it for consistency.

---
+++ Vienna Tests/RichXMLParserTests.swift
@@ -0,0 +1,60 @@
+//
+//  RichXMLParserTests.swift
+//  Vienna Tests
+//
+//  Copyright 2020
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import XCTest
+
+class RichXMLParserTests: XCTestCase {
+
+	override func setUpWithError() throws {
+		try super.setUpWithError()
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+	}
+
+	override func tearDownWithError() throws {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+		try super.tearDownWithError()
+	}
+
+	func testParseRichXML() {
+
+		//
+		// FIXME: Remove this?
+		//
+
+		/*
+		var success = false
+
+		// Test extracting feeds to an array
+		let url = URL(fileURLWithPath: "/Users/josh/test.rdf")
+		// old parser
+		let feedData = try Data(contentsOf: url)
+		let oldParser = RichXMLParser()
+		let oldParsedOK = oldParser.parseRichXML(feedData)
+
+		// new parser
+		if (oldParsedOK){
+
+		}
+
+		XCTAssertTrue(success, "Pass")
+		*/
+	}
+
+
+}
